5b0aa216b78eccbd49c47e0439b4d58c431313ea
Nick Seman <nick@nickseman.com>
2021-02-14 16:14:48 -0800

    chore: add shuffle to help command

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function listCommands(message) {
     .setTitle("All commands")
     .setDescription(
       `\`${prefix}p\`: Play a URL of a video or playlist, or search a term and play the first result. Adds to the end of the queue if present.\n
+      \`${prefix}p:shuffle\`: Shuffle a playlist while adding to the queue.\n
     \`${prefix}q\`: List the queue, including the currently playing item.\n
     \`${prefix}skip\`: Skips to the next item in queue.\n
     \`${prefix}stop\`: Stops and disconnects from the voice channel.\n
