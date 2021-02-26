260519aae805918f34e6b4b622d9b663f66f32fd
vphoebe <nick@stylefire.xyz>
2021-02-26 15:49:10 -0800

    chore: move handle message function to file

---
+++ commands/handleMessage.ts
@@ -0,0 +1,58 @@
+import Discord from "discord.js";
+import { prefix, shortcuts } from "../config.json";
+import {
+  execute,
+  skip,
+  stop,
+  listQueue,
+  changeVolume,
+  listCommands,
+  listShortcuts,
+} from "../commands";
+import { BotQueue } from "../types";
+
+export async function handleMessage(
+  message: Discord.Message,
+  botQueue: BotQueue
+) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  if (message.guild === null) return;
+
+  const serverQueue = botQueue.get(message.guild.id);
+
+  const args = message.content.split(" ");
+  const command = args[0].substring(1);
+  const detectedShortcut = shortcuts.find(
+    (shortcut) => shortcut.shortcut === command
+  );
+
+  if (detectedShortcut) {
+    message.content = `${prefix}${detectedShortcut.command}`;
+  }
+
+  if (message.content.startsWith(`${prefix}p`)) {
+    execute(message, serverQueue, botQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}skip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}q`)) {
+    listQueue(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}volume`)) {
+    changeVolume(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}help`)) {
+    listCommands(message);
+    listShortcuts(message);
+    return;
+  } else {
+    message.channel.send("You need to enter a valid command!");
+    listCommands(message);
+    return;
+  }
+}
--- index.ts
+++ index.ts
@@ -1,16 +1,8 @@
 import Discord from "discord.js";
-import { prefix, token, shortcuts } from "./config.json";
+import { token } from "./config.json";
 import presence from "./presence/presence";
 import { Queue, BotQueue } from "./types";
-import {
-  execute,
-  skip,
-  stop,
-  listQueue,
-  changeVolume,
-  listCommands,
-  listShortcuts,
-} from "./commands";
+import { handleMessage } from "./commands/handleMessage";
 
 const client = new Discord.Client();
 const botQueue: BotQueue = new Map<String, Queue>();
@@ -28,46 +20,6 @@ client.once("disconnect", () => {
   console.log("Disconnect!");
 });
 
-client.on("message", async (message) => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  if (message.guild === null) return;
-
-  const serverQueue = botQueue.get(message.guild.id);
-
-  const args = message.content.split(" ");
-  const command = args[0].substring(1);
-  const detectedShortcut = shortcuts.find(
-    (shortcut) => shortcut.shortcut === command
-  );
-
-  if (detectedShortcut)
-    message.content = `${prefix}${detectedShortcut.command}`;
-
-  if (message.content.startsWith(`${prefix}p`)) {
-    execute(message, serverQueue, botQueue);
-    return;
-  } else if (message.content.startsWith(`${prefix}skip`)) {
-    skip(message, serverQueue);
-    return;
-  } else if (message.content.startsWith(`${prefix}stop`)) {
-    stop(message, serverQueue);
-    return;
-  } else if (message.content.startsWith(`${prefix}q`)) {
-    listQueue(message, serverQueue);
-    return;
-  } else if (message.content.startsWith(`${prefix}volume`)) {
-    changeVolume(message, serverQueue);
-    return;
-  } else if (message.content.startsWith(`${prefix}help`)) {
-    listCommands(message);
-    listShortcuts(message);
-    return;
-  } else {
-    message.channel.send("You need to enter a valid command!");
-    listCommands(message);
-    return;
-  }
-});
+client.on("message", (message) => handleMessage(message, botQueue));
 
 client.login(token);
