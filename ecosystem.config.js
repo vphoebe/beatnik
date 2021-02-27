58136a03631b65eecb887471a0d9b408d55c9484
vphoebe <nick@stylefire.xyz>
2021-02-27 09:59:47 -0800

    fix: use https for git

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       host: "beatnik.sjw.zone",
       key: "deploy.key",
       ref: "origin/release",
-      repo: "git@github.com:vphoebe/beatnik.git",
+      repo: "https://github.com/vphoebe/beatnik.git",
       path: "/srv/beatnik-prod",
       "post-deploy":
         "npm install && npm run clean && npm run build && pm2 reload ecosystem.config.js --env production",
