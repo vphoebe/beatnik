5c2b0cdbea0e714c350949e2b58683d7c52af2d3
Nick Seman <me@vphoebe.dev>
2023-03-14 15:31:34 -0700

    chore: remove keepalive workaround

--- src/lib/connection.ts
+++ src/lib/connection.ts
@@ -26,28 +26,6 @@ export function createVoiceConnection(channel: VoiceBasedChannel) {
     }
   });
 
-  // https://github.com/discordjs/discord.js/issues/9185#issuecomment-1459083216 ????
-  const networkStateChangeHandler = (
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    oldNetworkState: any,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    newNetworkState: any
-  ) => {
-    const newUdp = Reflect.get(newNetworkState, "udp");
-    clearInterval(newUdp?.keepAliveInterval);
-  };
-
-  connection.on("stateChange", (oldState, newState) => {
-    Reflect.get(oldState, "networking")?.off(
-      "stateChange",
-      networkStateChangeHandler
-    );
-    Reflect.get(newState, "networking")?.on(
-      "stateChange",
-      networkStateChangeHandler
-    );
-  });
-
   return connection;
 }
 
