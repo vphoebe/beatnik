e8f4f8527c8a58af87d48f2704978bac9f587fb7
Nick Seman <me@vphoebe.dev>
2025-05-09 11:56:27 -0700

    feat: backup stream if no optimum format exists

--- src/lib/youtube/client.ts
+++ src/lib/youtube/client.ts
@@ -1,16 +1,35 @@
 import { Stream } from "node:stream";
 import { ClientType, Innertube, UniversalCache } from "youtubei.js";
 
+import { log } from "../logger.js";
+
 export const yt = await Innertube.create({
-  cache: new UniversalCache(false),
+  cache: new UniversalCache(true),
+  generate_session_locally: true,
   client_type: ClientType.WEB,
 });
 
 export const getYtStream = async (id: string) => {
-  const innertubeStream = await yt.download(id, {
-    type: "audio",
-    client: "WEB",
-  });
-
-  return Stream.Readable.fromWeb(innertubeStream);
+  try {
+    const stream = await yt.download(id, {
+      type: "audio",
+      codec: "opus",
+      quality: "best",
+      client: "WEB",
+    });
+    return Stream.Readable.fromWeb(stream);
+  } catch (_) {
+    // if no preferred stream found, just get whatever
+    log({
+      type: "INFO",
+      user: "BOT",
+      message: `Unable to find optimal stream for ${id}, falling back to first available.`,
+    });
+    const url = (await yt.getStreamingData(id, { format: "any" })).decipher();
+    const stream = (await fetch(url)).body;
+    if (!stream) {
+      throw new Error(`No compatible streams found for ${id}.`);
+    }
+    return Stream.Readable.fromWeb(stream);
+  }
 };
