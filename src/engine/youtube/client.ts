d252367e8fe7ad8d4178066c609428de02f4ec0a
Nick Seman <nick@nickseman.com>
2026-03-07 12:35:15 -0800

    feat: remove ad context option, rotate known-good player ids

--- src/engine/youtube/client.ts
+++ src/engine/youtube/client.ts
@@ -14,10 +14,31 @@ Platform.shim.eval = async (data, env) => {
   return new Function(code)();
 };
 
+// https://github.com/LuanRT/YouTube.js/issues/1146#issuecomment-4006959046
+const KNOWN_PIDS = [
+  "140dafda",
+  "4eecba16",
+  "00c52fa0",
+  "251ca12e",
+  "267b6435",
+  "32a343c8",
+  "48995d17",
+  "4c5cf06a",
+  "9f4cc5e4",
+  "a944b11f",
+];
+
+const getPlayerId = () => KNOWN_PIDS[Math.floor(Math.random() * KNOWN_PIDS.length)];
+
 const innertubePromise = Innertube.create({
   cache: new UniversalCache(false),
+  player_id: getPlayerId(),
 }).then((val) => {
-  log({ user: "BOT", type: "YT", message: `YouTube client established.` });
+  log({
+    user: "BOT",
+    type: "YT",
+    message: `YouTube client established, player ID: ${val.session.player?.player_id}`,
+  });
   return val;
 });
 
