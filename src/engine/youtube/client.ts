4dafa8cd5aa19bad936f09840b567082197f2d31
Nick Seman <me@vphoebe.dev>
2026-03-16 14:50:57 -0700

    chore(deps): upgrade yt lib and player id

--- src/engine/youtube/client.ts
+++ src/engine/youtube/client.ts
@@ -3,24 +3,12 @@ import { Innertube, Platform, UniversalCache } from "youtubei.js";
 import { getPlayerIdOverride } from "@helpers/environment";
 import { log } from "@helpers/logger";
 
-// https://github.com/LuanRT/YouTube.js/issues/1146#issuecomment-4006959046
-const KNOWN_PIDS = [
-  "140dafda",
-  "4eecba16",
-  "00c52fa0",
-  "251ca12e",
-  "267b6435",
-  "32a343c8",
-  "48995d17",
-  "4c5cf06a",
-  "9f4cc5e4",
-  "a944b11f",
-];
+const KNOWN_WORKING_PID = "487b9fc1";
 
 const getPlayerId = () => {
   const envOverride = getPlayerIdOverride();
 
-  return envOverride ?? KNOWN_PIDS[Math.floor(Math.random() * KNOWN_PIDS.length)];
+  return envOverride ?? KNOWN_WORKING_PID;
 };
 
 Platform.shim.eval = async (data, env) => {
